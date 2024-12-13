3485da10f57fd4528441a886e88d2444beea2cdd
joshPGibson <67116818+ikschalle@users.noreply.github.com>
2024-12-13 21:03:36 +0000

    Swap to Fabric & MC 1.20.1

---
+++ src/client/java/dev/nitrobunny/mcTinkering/client/McTinkeringClient.java
@@ -0,0 +1,10 @@
+package dev.nitrobunny.mcTinkering.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McTinkeringClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/nitrobunny/mcTinkering/McTinkering.java
@@ -0,0 +1,10 @@
+package dev.nitrobunny.mcTinkering;
+
+import net.fabricmc.api.ModInitializer;
+
+public class McTinkering implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
